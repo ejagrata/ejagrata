a7fb6b69d968fdb6725ed725cc38e8e103ae570b
mathbi03 <bibin.mathew.ap@nielsen.com>
2016-12-30 10:54:02 +0530

    @Added GET,POST,PUT,DELETE

--- src/main/java/org/ejagrata/controller/SchoolController.java
+++ src/main/java/org/ejagrata/controller/SchoolController.java
@@ -1,6 +1,8 @@
 package org.ejagrata.controller;
 
 import org.ejagrata.beans.SchoolBean;
+import org.ejagrata.entity.School;
+import org.ejagrata.repository.SchoolRepository;
 import org.ejagrata.service.SchoolService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,14 +16,34 @@ public class SchoolController {
 
 	@Autowired
 	private SchoolService schoolService;
+	
+	@Autowired
+	private SchoolRepository schoolRepository;
 
+	@RequestMapping(method=RequestMethod.GET,value="/school/{schoolId}")
+	public School getSchool(@PathVariable("schoolId") Integer schoolId	) {
+		return schoolRepository.findOne(schoolId);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET,value="/school")
+	public Iterable<School> getSchool() {
+		return schoolRepository.findAll();
+	}
+	
 	@RequestMapping(method = RequestMethod.POST, value = "/school")
-	public SchoolBean saveSchool(@RequestBody SchoolBean hospital) {
-		return null;
+	public School saveSchool(@RequestBody School school) {
+		return schoolRepository.save(school);
 	}
-
-	@RequestMapping("/school/{schoolId}")
-	public SchoolBean getSchool(@PathVariable("schoolId") Integer schoolId	) {
-		return schoolService.getSchool(schoolId);
+	
+	@RequestMapping(method = RequestMethod.PUT, value = "/school")
+	public School updateSchool(@RequestBody School school) {
+		return schoolRepository.save(school);
 	}
+	
+	@RequestMapping(method=RequestMethod.DELETE,value="/school/{schoolId}")
+	public void deleteSchool(@PathVariable("schoolId") Integer schoolId	) {
+		 schoolRepository.delete(schoolId);
+	}
+	
+	
 }
